f8cf8c107cd84cb32af7c0b2da87e4cce6994591
Kyle Banta <kbanta11@gmail.com>
2024-03-04 15:44:28 -0800

    first try adding steps..2

--- app/api/frame/start.ts
+++ app/api/frame/start.ts
@@ -2,7 +2,7 @@ import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/o
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
-async function getStartResponse(req: NextRequest): Promise<NextResponse> {
+async function getResponse(req: NextRequest): Promise<NextResponse> {
     const body: FrameRequest = await req.json();
     const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
   
@@ -33,3 +33,10 @@ async function getStartResponse(req: NextRequest): Promise<NextResponse> {
       }),
     );
   }
+
+  export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse(req);
+  }
+  
+  export const dynamic = 'force-dynamic';
+  
