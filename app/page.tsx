bc0e7c2afb5f362807c6a20b170c05bd27e327cb
Kyle Banta <kbanta11@gmail.com>
2024-03-04 16:14:00 -0800

    try 4

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const frameMetadata = getFrameMetadata({
     src: `${NEXT_PUBLIC_URL}/park-3.png`,
     aspectRatio: '1:1',
   },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame/start`,
 });
 
 export const metadata: Metadata = {
