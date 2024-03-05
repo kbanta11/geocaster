d9efcf5a39bc39a60f82f9b70e78782366dc5429
Kyle Banta <kbanta11@gmail.com>
2024-03-05 13:01:22 -0800

    try updated second frame

--- app/api/frame/start/route.ts
+++ app/api/frame/start/route.ts
@@ -28,26 +28,28 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: 'Story time!',
+          label: '⬅️',
+        },
+        {
+          label: '➡️',
+        },
+        {
+          label: 'Guess',
         },
         {
           action: 'link',
-          label: 'Link to Google',
-          target: 'https://www.google.com',
-        },
-        {
-          label: 'Redirect to pictures',
-          action: 'post_redirect',
+          label: 'Leaderboard',
+          target: 'geocaster.xyz'
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-3.png`,
+        src: `${NEXT_PUBLIC_URL}/earth.png`,
         aspectRatio: '1:1',
       },
       input: {
-        text: 'Tell me a boat story',
+        text: 'Guess the Country',
       },
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame/start`,
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame/0`,
     }),
   );
 }
