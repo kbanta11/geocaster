1e2f19011f0ddcbea8557f6ac02e3633119e8cd2
Kyle Banta <kbanta11@gmail.com>
2024-03-05 08:21:47 -0800

    revert to test

--- app/api/frame/start/route.ts
+++ app/api/frame/start/route.ts
@@ -13,31 +13,39 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
+  if (message?.input) {
+    text = message.input;
+  }
+
+  if (message?.button === 3) {
+    return NextResponse.redirect(
+      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      { status: 302 },
+    );
+  }
+
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: '<-',
-        },
-        {
-          label: '->',
-        },
-        {
-          label: 'Submit',
-          action: 'post_redirect',
+          label: 'Story time!',
         },
         {
           action: 'link',
-          label: 'Leaderboard',
-          target: 'geocaster.xyz/leaderboard'
-        }
+          label: 'Link to Google',
+          target: 'https://www.google.com',
+        },
+        {
+          label: 'Redirect to pictures',
+          action: 'post_redirect',
+        },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/earth.png`,
+        src: `${NEXT_PUBLIC_URL}/park-3.png`,
         aspectRatio: '1:1',
       },
       input: {
-        text: 'Guess the country',
+        text: 'Tell me a boat story',
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame/start`,
     }),
